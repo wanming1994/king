ed1a87541150b55854946456ad89a7cb6940be9c
wanming <978676783@qq.com>
2017-12-03 13:21:05 +0800

    分享

---
+++ pages/member/share/share.js
@@ -0,0 +1,53 @@
+// pages/home/home.js
+
+var   app = getApp(),
+  util = require("../../../utils/util.js")
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  //分享
+  shareFriend: function (res) {
+    var that = this;
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+
+    }
+    return {
+      title: '大王纸尿裤',
+      path: 'pages/home/home',
+      success: function (res) {
+        // 转发成功
+        wx.showToast({
+          title: '转发成功',
+          icon: 'success'
+        })
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }
+})
