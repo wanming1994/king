43eb50bfcbaa8f27a3cde9024746274782c6bcaf
wanming <wanming@tiaohuo.com>
2017-12-05 16:59:32 +0800

    样式

--- pages/member/member.js
+++ pages/member/member.js
@@ -2,58 +2,58 @@ let app = getApp(),
   util = require("../../utils/util.js")
 Page({
   data: {
-    
+
   },
   onLoad: function (options) {
-    
+
   },
   onReady: function () {
-    
+
   },
   onShow: function () {
-    
+
   },
-  getGoods(){
+  getGoods() {
     util.navigateTo({
       url: 'getGoods/getGoods',
     })
   },
   //个人资料
-  toInfo:function(){
+  toInfo: function () {
     util.navigateTo({
       url: 'info/info',
     })
   },
   //积分兑换
-  goExchange:function(){
+  goExchange: function () {
     util.navigateTo({
       url: 'exchange/exchange',
     })
   },
   //我的影响力
-  toMyInfluence:function(){
+  toMyInfluence: function () {
     util.navigateTo({
       url: 'influence/influence',
     })
   },
   //分享
-  goShare:function(){
+  goShare: function () {
     util.navigateTo({
       url: 'share/share',
     })
   },
   //绑定手机
-  bindPhone:function(){
+  bindPhone: function () {
     util.navigateTo({
       url: 'bind/bind',
     })
   },
 
-    //我的订单
-    toOrder: function (e) {
-        var id = e.currentTarget.dataset.current
-        util.navigateTo({
-            url: 'order/order?id=' + id,
-        })
-    },
+  //我的订单
+  toOrder: function (e) {
+    var id = e.currentTarget.dataset.current
+    util.navigateTo({
+      url: 'order/order?id=' + id,
+    })
+  }
 })
