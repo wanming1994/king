eb27568ac26ec3ce4dfeff81dd07faf2149e2fbd
wwh <214032145@qq.com>
2017-12-22 00:11:28 +0800

    getgoods

--- pages/member/getGoods/getGoods.js
+++ pages/member/getGoods/getGoods.js
@@ -10,27 +10,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    selectData: {
+      count: 0,
+      name: '',
+      num: 0
+    },
+    sum: 0,
+    data: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      new order(function(){
-
-      }).myEcoupons({
-
+    new order(res => {
+      this.setData({
+        data: res.data.data,
+        sum: res.data.sum
       })
-      new order(function(){
+    }).myEcoupons({
 
-      }).goodsAttribute({
-        ecouponId:1
-      })
+    })
+    new order(function () {
+
+    }).goodsAttribute({
+      ecouponId: 1
+    })
   },
 
 
-  select: function () {
+  select: function (e) {
+    const { id, count, name } = e.currentTarget.dataset
+    if (Number(count) === 0) {
+      util.errShow("该类型暂无可提")
+      return
+    }
     this.setData({
       showAction: true
     })
@@ -49,48 +63,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
