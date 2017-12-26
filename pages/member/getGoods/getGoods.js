7c75bb008fddc9eec0e60b02afd16e0fda72ec05
wwh <wangwenhao@tiaohuo.com>
2017-12-26 17:28:11 +0800

    .

--- pages/member/getGoods/getGoods.js
+++ pages/member/getGoods/getGoods.js
@@ -8,291 +8,302 @@ var password = require('../../../service/common.js');
 // pages/member/getGoods/getGoods.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    selectData: {
-      count: 0,
-      name: '',
-      num: 0
-    },
-    selectDataList: [],
-    sum: 0,
-    data: [],
-    specifications: {}
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    new order(res => {
-      this.setData({
-        data: res.data.data,
-        sum: res.data.sum,
-        address: res.data.address
-      })
-    }).myEcoupons()
-
-    new cart(function(){
-
-    }).list()
-  },
-
-
-  //收货地址
-  chooseAddress: function () {
-    var that = this;
-    try {
-      wx.chooseAddress({
-        success: function (res) {
-          console.log(res)
-          new receiver(function (data) {
-            wx.showToast({
-              title: '保存成功',
-              icon: 'success'
-            })
-
-            new order(res => {
-              that.setData({
-                address: res.data.address
-              })
-            }).myEcoupons()
-
-          }).save({
-            provinceName: res.provinceName,
-            cityName: res.cityName,
-            countyName: res.countyName,
-            detailInfo: res.detailInfo,
-            userName: res.userName,
-            telNumber: res.telNumber,
-            nationalCode: res.nationalCode,
-            postalCode: res.postalCode
-          })
-        },
-        fail: function (err) {
-          if (err.errMsg.indexOf('auth') > -1) {
-            wx.showModal({
-              title: '提示',
-              content: '未授予地址权限，是否前往设置',
-              success: function (res) {
-                if (res.confirm) {
-                  wx.openSetting()
-                }
-              }
-            })
-          }
-        }
-      })
-    } catch (e) {
-      util.errShow('微信版本过低')
-    }
-  },
-
-  checkout(e) {
-    const { id, pid } = e.currentTarget.dataset
-    const selectData = this.data.selectData
-    const selectSpList = this.data.selectData.specifications
-    for (let i = 0; i < selectSpList.length; i++) {
-      if (selectSpList[i].specification_id == pid) {
-        for (let j = 0; j < selectSpList[i].valueList.length; j++) {
-          if (selectSpList[i].valueList[j].id == id) {
-            selectData.specificationsSelect[pid] = selectSpList[i].valueList[j]
-            selectData.num = 0
-            this.setData({
-              selectData: selectData
-            })
-            break;
-          }
-        }
-      }
-      break;
-    }
-  },
-  select: function (e) {
-    const { id, ecouponsid, ecouponsname, count, name, ecouponstype } = e.currentTarget.dataset
-    if (parseInt(count) === 0) {
-      util.errShow("该类型暂无可提")
-      return
-    }
-
-    this.getSpecifications(ecouponsid).then(res => {
-      const localSelect = this.data.selectDataList.filter(item => {
-        return item.id == id && item.ecouponsid == ecouponsid && item.specificationsSelect[res[0].specification_id].id == res[0].valueList[0].id
-      })
-      this.setData({
-        showAction: true,
+    /**
+     * 页面的初始数据
+     */
+    data: {
         selectData: {
-          count: count,
-          id: id,
-          ecouponsid: ecouponsid,
-          name: ecouponsname,
-          num: 0,
-          ecouponstype: ecouponstype,
-          specifications: res,
-          specificationsSelect: {
-            [res[0].specification_id]: res[0].valueList[0]
-          }
+            count: 0,
+            name: '',
+            num: 0
         },
+        selectDataList: [],
+        sum: 0,
+        data: [],
+        specifications: {},
+        cartList: [],
+        updateCartListFlag: true,
+    },
 
-      })
-    })
-
-  },
-
-  //弹出框toggle
-
-  toggleMask(e) {
-    this.setData({
-      showAction: !this.data.showAction,
-      buyType: e.currentTarget.dataset.type,
-      _swiper: this.data._swiper,
-      selectData: {}
-    })
-  },
-
-  revisenum(e) {
-    let result, selectDataList = this.data.selectDataList, sum = 0
-    const { btype, ltype, index } = e.currentTarget.dataset
-    let { num, count, id, ecouponsid } = ltype ? this.data.selectDataList[index] : this.data.selectData
-    for (let i = 0; i < selectDataList.length; i++) {
-      if (id == selectDataList[i].id && ecouponsid == selectDataList[i].ecouponsid) {
-        sum += parseInt(selectDataList[i].num)
-      }
-    }
-    sum -= num
-    count = count - sum
-    switch (btype) {
-      case 'reduce':
-        result = num - 1
-        break
-      case 'input':
-        result = e.detail.value
-        break
-      case 'add':
-        result = num + 1
-        break
-      default:
-        result = 0
-    }
-    if (result >= 0 && result <= count) {
-      if (ltype) {
-        this.data.selectDataList[index].num = result
-        this.setData({
-          selectDataList: this.data.selectDataList
-        })
-        return
-      }
-      this.setData({
-        'selectData.num': result
-      })
-      return
-    }
-    ltype ? this.setData({
-      selectDataList: this.data.selectDataList
-    }) : this.setData({
-      'selectData.num': num
-    })
-  },
-  paySubmit() {
-
-    //加入购物车
-    new cart(function(){
-
-    }).add({
-      ecouponsType:1,
-      ecouponsId:2,
-      ecouponsCnt:3,
-      specificationId:7,
-      addressId:16
-    })
-    // const selectData = this.data.selectData
-    // const selectDataList = this.data.selectDataList || []
-    // for (let i = 0; i < selectDataList.length; i++) {
-    //   if (selectDataList[i].id == selectData.id && selectDataList[i].ecouponsid == selectData.ecouponsid) {
-    //     let ssSelect = selectDataList[i].specificationsSelect
-    //     let selectssSelect = selectData.specificationsSelect
-    //     let sign = true
-    //     for (let key in ssSelect) {
-    //       if (selectssSelect[key].id != ssSelect[key].id) {
-    //         sign = false
-    //         break;
-    //       }
-    //     }
-    //     if (sign) {
-    //       selectDataList[i] = selectData
-    //       this.setData({
-    //         selectDataList: selectDataList,
-    //         showAction: false
-    //       })
-    //       return
-    //     }
-    //   }
-    // }
-    // selectDataList.push(selectData)
-    // this.setData({
-    //   selectDataList: selectDataList,
-    //   showAction: false
-    // })
-  },
-  getSpecifications(ecouponId) {
-    return new Promise((resolve, reject) => {
-      if (this.data.specifications[ecouponId]) {
-        resolve(this.data.specifications[ecouponId])
-      } else {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
         new order(res => {
-          this.data.specifications[ecouponId] = res.data
-          this.setData({
-            specifications: this.data.specifications
-          })
-          resolve(res.data)
-        }).goodsAttribute({
-          ecouponId: ecouponId
+            this.setData({
+                data: res.data.data,
+                sum: res.data.sum,
+                address: res.data.address
+            })
+        }).myEcoupons()
+        this.getCartList().then(res => {
+
         })
-      }
-    })
-  },
+    },
+    getCartList() {
+        return new Promise((resolve, reject) => {
+            if (!this.data.updateCartListFlag) {
+                resolve(this.data.cartList)
+            }
+            new cart(res => {
+                this.data.updateCartListFlag = false
+                wx.hideLoading()
+                this.setData({
+                    cartList: res.cartList
+                })
+                resolve(res)
+            }).list()
+        })
+    },
+    updateCart(cid, count) {
+        return new Promise((resolve, reject) => {
+            if (this.data.address.id) reject()
+            new cart(res => {
+                this.data.updateCartListFlag = true
+                resolve(res)
+            }).edit({
+                cid: cid,
+                count: count,
+                addressId: this.data.address.id
+            })
+        })
+    },
+    getSpecifications(ecouponId) {
+        return new Promise((resolve, reject) => {
+            if (this.data.specifications[ecouponId]) {
+                resolve(this.data.specifications[ecouponId])
+            } else {
+                new order(res => {
+                    this.data.specifications[ecouponId] = res.data[0].valueList
+                    this.setData({
+                        specifications: this.data.specifications
+                    })
+                    resolve(res.data[0].valueList)
+                }).goodsAttribute({
+                    ecouponId: ecouponId
+                })
+            }
+        })
+    },
+    //收货地址
+    chooseAddress: function () {
+        var that = this;
+        try {
+            wx.chooseAddress({
+                success: function (res) {
+                    console.log(res)
+                    new receiver(function (data) {
+                        wx.showToast({
+                            title: '保存成功',
+                            icon: 'success'
+                        })
+                        new order(res => {
+                            that.setData({
+                                address: res.data.address
+                            })
+                        }).myEcoupons()
+                    }).save({
+                        provinceName: res.provinceName,
+                        cityName: res.cityName,
+                        countyName: res.countyName,
+                        detailInfo: res.detailInfo,
+                        userName: res.userName,
+                        telNumber: res.telNumber,
+                        nationalCode: res.nationalCode,
+                        postalCode: res.postalCode
+                    })
+                },
+                fail: function (err) {
+                    if (err.errMsg.indexOf('auth') > -1) {
+                        wx.showModal({
+                            title: '提示',
+                            content: '未授予地址权限，是否前往设置',
+                            success: function (res) {
+                                if (res.confirm) {
+                                    wx.openSetting()
+                                }
+                            }
+                        })
+                    }
+                }
+            })
+        } catch (e) {
+            util.errShow('微信版本过低')
+        }
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    checkout(e) {
+        const {id, index} = e.currentTarget.dataset
+        const selectData = this.data.selectData
+        const cartList = this.data.cartList
+        // for (let i = 0; i < cartList.length; i++) {
+        //     if (cartList[i].id === id) {
+        //
+        //         break
+        //     }
+        // }
+    },
+    select: function (e) {
+        const {id, ecouponsid, ecouponsname, count, name, ecouponstype} = e.currentTarget.dataset
+        if (parseInt(count) === 0) {
+            util.errShow("该类型暂无可提")
+            return
+        }
+        this.getSpecifications(ecouponsid).then(sList => {
+            const localSelect = this.data.cartList.filter(item => {
+                return item.goods_specifition_ids == sList[0].id
+            })
+            this.setData({
+                showAction: true,
+                selectData: {
+                    count: count,//该类商品库存
+                    id: id,
+                    ecouponsid: ecouponsid,
+                    name: ecouponsname,
+                    num: [localSelect.length > 0 ? localSelect[0].number : 0],//已选数量，数组：每种规格的数量，用sSelect获取当前选中index
+                    ecouponstype: ecouponstype,
+                    specifications: sList,//规格列表
+                    specificationsSelect: 0,//当前选中规格
+                }
+            })
+        })
+    },
 
-  },
+    //弹出框toggle
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    toggleMask(e) {
+        this.setData({
+            showAction: !this.data.showAction,
+            buyType: e.currentTarget.dataset.type,
+            _swiper: this.data._swiper,
+            selectData: {}
+        })
+    },
 
-  },
+    revisenum(e) {
+        let result, selectDataList = this.data.selectDataList, sum = 0
+        const {btype, ltype, index} = e.currentTarget.dataset
+        let {num, count, id, ecouponsid} = ltype ? this.data.selectDataList[index] : this.data.selectData
+        for (let i = 0; i < selectDataList.length; i++) {
+            if (id == selectDataList[i].id && ecouponsid == selectDataList[i].ecouponsid) {
+                sum += parseInt(selectDataList[i].num)
+            }
+        }
+        sum -= num
+        count = count - sum
+        switch (btype) {
+            case 'reduce':
+                result = num - 1
+                break
+            case 'input':
+                result = e.detail.value
+                break
+            case 'add':
+                result = num + 1
+                break
+            default:
+                result = 0
+        }
+        if (result >= 0 && result <= count) {
+            if (ltype) {
+                this.data.selectDataList[index].num = result
+                this.setData({
+                    selectDataList: this.data.selectDataList
+                })
+                return
+            }
+            this.setData({
+                'selectData.num': result
+            })
+            return
+        }
+        ltype ? this.setData({
+            selectDataList: this.data.selectDataList
+        }) : this.setData({
+            'selectData.num': num
+        })
+    },
+    paySubmit() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+        //加入购物车
+        new cart(function () {
 
-  },
+        }).add({
+            ecouponsType: 1,
+            ecouponsId: 2,
+            ecouponsCnt: 3,
+            specificationId: 7,
+            addressId: 16
+        })
+        // const selectData = this.data.selectData
+        // const selectDataList = this.data.selectDataList || []
+        // for (let i = 0; i < selectDataList.length; i++) {
+        //   if (selectDataList[i].id == selectData.id && selectDataList[i].ecouponsid == selectData.ecouponsid) {
+        //     let ssSelect = selectDataList[i].specificationsSelect
+        //     let selectssSelect = selectData.specificationsSelect
+        //     let sign = true
+        //     for (let key in ssSelect) {
+        //       if (selectssSelect[key].id != ssSelect[key].id) {
+        //         sign = false
+        //         break;
+        //       }
+        //     }
+        //     if (sign) {
+        //       selectDataList[i] = selectData
+        //       this.setData({
+        //         selectDataList: selectDataList,
+        //         showAction: false
+        //       })
+        //       return
+        //     }
+        //   }
+        // }
+        // selectDataList.push(selectData)
+        // this.setData({
+        //   selectDataList: selectDataList,
+        //   showAction: false
+        // })
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  }
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
