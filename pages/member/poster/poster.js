c60dbbdb12572a06208a81308a5dfa9f63755c13
宛明 <wanming@tiaohuo.com>
2018-04-20 10:23:39 +0800

    海报生成bug

--- pages/member/poster/poster.js
+++ pages/member/poster/poster.js
@@ -35,7 +35,7 @@ Page(Object.assign({}, {
       success: function (res) {
         wx.request({
           url: 'https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=' + res.data.access_token,
-          method:'POST',
+          method: 'POST',
           data: {
             path: "pages/home/home",
             width: 430
@@ -65,61 +65,68 @@ Page(Object.assign({}, {
     })
 
     var res1 = '/resources/images/member/poster_bg.png'
-    wx.getSystemInfo({
+    wx.downloadFile({
+      url: that.data.avatarUrl,
       success: function (res) {
-        var w = res.windowWidth;
-        var h = res.windowHeight;
-        const ctx = wx.createCanvasContext('myCanvas')
-        ctx.setFillStyle('rgb(255, 255, 255)')
-        ctx.fillRect(0, 0, w, h)
-        ctx.drawImage(res1, 0, 0, w, h)//背景图大
-        ctx.save();
-        ctx.beginPath()
-        ctx.arc(0.15 * w + 0.15 * w / 2, 0.56 * h + 0.15 * w / 2, 0.15 * w / 2, 0, 2 * Math.PI);
-        ctx.setStrokeStyle('#ffffff')
-        ctx.clip();
-        ctx.drawImage(that.data.avatarUrl, 0.15 * w, 0.56 * h, 0.15 * w, 0.15 * w)//小程序二维码
-        ctx.stroke();
-        ctx.closePath();
-        ctx.restore();
+        var headImg = res.tempFilePath
+        wx.getSystemInfo({
+          success: function (res) {
+            var w = res.windowWidth;
+            var h = res.windowHeight;
+            const ctx = wx.createCanvasContext('myCanvas')
+            ctx.setFillStyle('rgb(255, 255, 255)')
+            ctx.fillRect(0, 0, w, h)
+            ctx.drawImage(res1, 0, 0, w, h)//背景图大
+            ctx.save();
+            ctx.beginPath()
+            ctx.arc(0.15 * w + 0.15 * w / 2, 0.56 * h + 0.15 * w / 2, 0.15 * w / 2, 0, 2 * Math.PI);
+            ctx.setStrokeStyle('#ffffff')
+            ctx.clip();
+            ctx.drawImage(headImg, 0.15 * w, 0.56 * h, 0.15 * w, 0.15 * w)//小程序二维码
+            ctx.stroke();
+            ctx.closePath();
+            ctx.restore();
 
-        ctx.setTextAlign('left')
-        ctx.setFillStyle('rgb(43, 43, 43)')
-        ctx.setFontSize(14)
-        ctx.fillText('我是' + app.globalData.memberInfo.userInfo.nickName, 0.20 * h, 0.60 * h)
-        ctx.setFillStyle('rgb(137, 110, 87)')
-        ctx.setFontSize(16)
-        ctx.fillText('邀请您加入大王', 0.20 * h, 0.63 * h)
+            ctx.setTextAlign('left')
+            ctx.setFillStyle('rgb(43, 43, 43)')
+            ctx.setFontSize(14)
+            ctx.fillText('我是' + app.globalData.memberInfo.userInfo.nickName, 0.20 * h, 0.60 * h)
+            ctx.setFillStyle('rgb(137, 110, 87)')
+            ctx.setFontSize(16)
+            ctx.fillText('邀请您加入大王', 0.20 * h, 0.63 * h)
 
-        ctx.drawImage(that.data.avatarUrl, 0.15 * h, 0.68 * h, 0.24 * w, 0.24 * w)//小程序二维码
-        ctx.draw();
-        // setTimeout(function () {
-        //   wx.canvasToTempFilePath({
-        //     //通过id 指定是哪个canvas
-        //     canvasId: 'myCanvas',
-        //     success(res) {
-        //       //成功之后保存到本地
-        //       console.log(res)
-        //       wx.saveImageToPhotosAlbum({
-        //         filePath: res.tempFilePath,
-        //         success: function (res) {
-        //           wx.showToast({
-        //             title: '保存成功',
-        //             icon: 'success',
-        //             duration: 2000
-        //           })
-        //         },
-        //         fail: function (res) {
-        //           console.log(res)
-        //         }
-        //       })
-        //     }
-        //   })
-        // }, 5000)
-      }, fail: function (e) {
-        console.log(e)
+            ctx.drawImage(that.data.avatarUrl, 0.15 * h, 0.68 * h, 0.24 * w, 0.24 * w)//小程序二维码
+            ctx.draw();
+            // setTimeout(function () {
+            //   wx.canvasToTempFilePath({
+            //     //通过id 指定是哪个canvas
+            //     canvasId: 'myCanvas',
+            //     success(res) {
+            //       //成功之后保存到本地
+            //       console.log(res)
+            //       wx.saveImageToPhotosAlbum({
+            //         filePath: res.tempFilePath,
+            //         success: function (res) {
+            //           wx.showToast({
+            //             title: '保存成功',
+            //             icon: 'success',
+            //             duration: 2000
+            //           })
+            //         },
+            //         fail: function (res) {
+            //           console.log(res)
+            //         }
+            //       })
+            //     }
+            //   })
+            // }, 5000)
+          }, fail: function (e) {
+            console.log(e)
+          }
+        })
       }
     })
+
 
 
 
